1528ec33d63d12e75fbff9e507ac719d577f4255
HugoEmDu <hhugoduarte31@gmail.com>
2026-05-16 17:56:15 -0300

    feat: estructura inicial con build local y test

---
+++ app.js
@@ -0,0 +1,6 @@
+function saludar() {
+    return "Hola Mundo";
+}
+
+// Exportamos la función para que el archivo de pruebas la pueda leer
+module.exports = saludar;
---
+++ app.test.js
@@ -0,0 +1,5 @@
+const saludar = require('./app');
+
+test('Validar que el saludo sea Hola Mundo', () => {
+    expect(saludar()).toBe("Hola Mundo");
+});
