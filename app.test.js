213f29ffd0e526029a38abfabbe2281c65fa59d3
HugoEmDu <hhugoduarte31@gmail.com>
2026-06-14 21:21:11 -0300

    fix: desfasaje entre index, app y test [ORFu7uyV]

--- app.test.js
+++ app.test.js
@@ -1,5 +1,21 @@
+/** @jest-environment jsdom */
 const saludar = require('./app');
+const fs = require('fs');
+const path = require('path');
+const html = fs.readFileSync(path.resolve(__dirname, './index.html'), 'utf8');
 
-test('Validar que el saludo sea Hola Mundo', () => {
-    expect(saludar()).toBe("Hola Mundo UTN 3.0");
+describe('Pruebas de Integración y UI básica', () => {
+
+    beforeEach(() => {
+        document.documentElement.innerHTML = html.toString();
+    });
+
+    test('Validar que el saludo sea Hola Mundo', () => {
+        expect(saludar()).toBe("Hola Mundo UTN 5.0");
+    });
+
+    test('El archivo index.html contiene el mensaje correcto renderizado', () => {
+        const cuerpoTexto = document.body.innerHTML;
+        expect(cuerpoTexto).toContain('Hola Mundo UTN 5.0');
+    });
 });
